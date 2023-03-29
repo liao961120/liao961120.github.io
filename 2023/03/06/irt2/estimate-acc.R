a90cd16773aa9724eef87b4d4a58586265d1657f
liao961120 <liao961120@users.noreply.github.com>
2023-03-29 05:41:07 +0000

    deploy: 21f01359fdc2d50b534dd7b161727dcac140c9c6

--- 2023/03/06/irt2/estimate-acc.R
+++ 2023/03/06/irt2/estimate-acc.R
@@ -24,7 +24,8 @@ plot_estimate = function(D, A, item_eff, subj_eff) {
   n_param = n_item + n_subj
   true_eff = c(D, A)
   est_eff = c(-item_eff, subj_eff)
-  y_lim = c( min(true_eff, est_eff)-.3, max(true_eff, est_eff)+.3 )
+  y_lim = max( abs(c(true_eff, est_eff)) )
+  y_lim = c( -y_lim-.1, y_lim+.1 )
   cols = c( rep(2, n_item), rep(4, n_subj) )
   plot( 1, type="n", ylim=y_lim, xlim=c(1, n_param+1), ylab="Effect" )
   abline( h=0, lty="dashed", col="grey" )
